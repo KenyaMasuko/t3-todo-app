0955d1755363af82a49a95aa712c4efd93c88dfa
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2022-10-29 11:02:20 +0900

    zodでバリデーションした内容のエラーメッセージをフォーマット

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,11 +1,22 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import type { Context } from "./context";
 import superjson from "superjson";
+import { ZodError } from "zod";
 
 export const t = initTRPC.context<Context>().create({
   transformer: superjson,
-  errorFormatter({ shape }) {
-    return shape;
+  // Errorをフォーマット (https://trpc.io/docs/v10/error-formatting)
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
   },
 });
 
